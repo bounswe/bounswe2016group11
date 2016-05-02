717f119d72974ade1e34c2dd2625770e2e7f2e9e
Melih Barsbey <melih.barsbey@gmail.com>
2016-05-02 19:36:16 +0300

    SPARQL query

--- group11/src/servlet/Melih.java
+++ group11/src/servlet/Melih.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 //import com.hp.hpl.jena.rdf.model.Model;
+import java.sql.*;
 
 /**
  * Servlet implementation class Melih
@@ -25,16 +26,18 @@ public class Melih extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Integer menu = 0;
+		//Integer menu = 0;
 		PrintWriter out = response.getWriter();
 		String my_query = request.getParameter("my_query");
-		if (my_query == null){out.println("<form action='Melih' method='get'> Please enter a query:<br><input type='text' name='my_query'><br><br><input type='submit' value='Submit'></form>");}
-		else{out.println("<html>");
+		out.println("<html>");
 		out.println("<body>");
-		out.println("<p>" + my_query + "</p>");
+		if (my_query == null){out.println("<form action='Melih' method='get'> Please enter a query:<br><input type='text' name='my_query'><br><br><input type='submit' value='Submit'></form>");}
+		else{out.println("<p>" + my_query + "</p>");}
+		out.println("<br>");
+		out.println("<form action='https://query.wikidata.org/sparql?query=SELECT ?countryLabel ?capitalLabel WHERE {?country wdt:P31 wd:Q3624078. ?capital wdt:P1376 ?country. SERVICE wikibase:label{bd:serviceParam wikibase:language 'en'. }}' method='get'> Click below to send your query:<input type='submit' value='Submit'></form>");
+		out.println("<br>");
+		out.println("<form action='Melih' method='get'> Please click here to initialize the database: <input type='submit' value='Click'></form>");
 		out.println("</body>");
-		out.println("</html>");}
-	}
-	
-	
+		out.println("</html>");
+	}	
 }
