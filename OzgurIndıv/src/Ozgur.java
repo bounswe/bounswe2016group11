d06121ed73b5300ba2a755e95a68564e2fafdde3
Ozgur Akyazi <ozgur.akyazi@boun.eud.tr>
2016-05-08 12:36:53 +0300

    The queries are ready to use. And topic of mine is films, date of films and actors

--- OzgurIndıv/src/Ozgur.java
+++ OzgurIndıv/src/Ozgur.java
@@ -17,16 +17,14 @@ import org.json.JSONObject;
 @WebServlet("/Ozgur")
 public class Ozgur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    public Ozgur_HtmlToJson wikiQ;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Ozgur() {
         super();
-        // TODO Auto-generated constructor stub
-    
         System.out.println();
-    
+        wikiQ = new Ozgur_HtmlToJson();
         
     }
 
@@ -36,12 +34,15 @@ public class Ozgur extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter theo = response.getWriter();
 		theo.println("My name is Özgür.");
-		JSONArray theaArr = Ozgur_HtmlToJson.getJSONResponseForQuery(Ozgur_HtmlToJson.basicQuery);
+		String[] thestr = new String[2];
+		thestr[0] = "2010";
+		thestr[0] = "Christian Bale";
+		JSONArray theaArr = queryWiki(1,thestr);
 		//String thefour = theaArr.getJSONObject(4).getJSONObject("countryLabel").getString("value");
 		
 		theo.append(indexPage());
 		theo.append(theaArr.toString());
-		
+	
 	}
 
 	/**
@@ -58,6 +59,26 @@ public class Ozgur extends HttpServlet {
 				"<input type=\"submit\" name=\"submit <br>\">"+
 				"</form> ";
 		return result;
+	}
+	
+	/*
+	 * Used to query wikidata, by using Ozgur_HtmlToJson class
+	 * @typeOfQuery if 1 -> Query the films that the actor specified in the @data[0] played in.
+	 * if 2 -> Query the actors in the film @data[0]
+	 * if 3 -> Query the films in year @data[0] played by actor @data[1]
+	 * */
+	protected JSONArray queryWiki(int typeOfQuery, String[] data) {
+		
+		if(typeOfQuery == 1){
+			return wikiQ.queryWithActor(data[0]);
+		}
+		else if(typeOfQuery ==2){
+			return wikiQ.queryWithFilm(data[0]);
+		}
+		else if(typeOfQuery == 3){
+			return wikiQ.queryWithActorAndYear(data[0], data[1]);
+		}
+		return null;
 	}
 
 }
--- OzgurIndıv/src/Ozgur_HtmlToJson.java
+++ OzgurIndıv/src/Ozgur_HtmlToJson.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.json.JSONArray;
@@ -8,15 +9,71 @@ import org.json.JSONObject;
 
 public class Ozgur_HtmlToJson {
 	
-	final private static String base = "https://query.wikidata.org/sparql?query=";
-	final static String basicQuery = "SELECT%20?countryLabel%20?capitalLabel%20WHERE{%20?capital%20wdt:P1376%20?country.%20?country%20wdt:P31%20wd:Q3624078.%20SERVICE%20wikibase:label%20{bd:serviceParam%20wikibase:language%20%22en%22%20.}}&format=json";
+	final private  String base = "https://query.wikidata.org/sparql?query=";
+	final String queryActor = "SELECT ?film ?filmLabel WHERE { "
+			+ "?film wdt:P31 wd:Q11424. "
+			+ "?item wdt:P106 wd:Q33999 ."
+			+ " ?film wdt:P161 ?item. "
+			+ "?item rdfs:label  \"theActor\"@en. "
+			+ "SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" }}";
+	final String queryActorYear = "SELECT ?item ?itemLabel WHERE "
+			+ "{ ?item wdt:P577 ?date FILTER( ?date "
+			+ "= \"theYear-01-01T00:00:00Z\"^^xsd:dateTime). ?item wdt:P31 wd:Q11424 . "
+			+ "?item wdt:P161 ?person."
+			+ " ?person wdt:P31 wd:Q5. "
+			+ "?person rdfs:label  ?name filter(?name = \"theActor\"@en). "
+			+ "SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" }}";
+	final String queryFilm = "SELECT %3Fitem %3FitemLabel "
+			+ "WHERE { %3Ffilm wdt%3AP31 wd%3AQ11424. "
+			+ "%3Ffilm rdfs%3Alabel  %3Fname filter(%3Fname %3D \"theFilm\"%40en). "
+			+ "%3Ffilm wdt%3AP161 %3Fitem. "
+			+ "%3Fitem wdt%3AP106 wd%3AQ33999 .  "
+			+ "SERVICE wikibase%3Alabel { bd%3AserviceParam wikibase%3Alanguage \"en\" } }";
+	private JSONArray getJSONResponseForQuery(String theQuery){
+		theQuery = theQuery.replace(":", "%3A");
+		theQuery = theQuery.replace("=", "%3D");
+		theQuery = theQuery.replace("@", "%40");
+		theQuery = theQuery.replace("?", "%3F");
+		//theQuery = theQuery.replace("\"", "'");
+		theQuery = theQuery.replace(" ", "%20");
+		//theQuery = theQuery.replace("'", "%22");
+		System.out.println(base + theQuery);
+		return getJSONResponseURL(base + theQuery + "%20&format=json");
+	}
 	
-	public static JSONArray getJSONResponseForQuery(String theQuery){
-		theQuery.replace("\"", "'");
-		theQuery.replace(" ", "%20");
-		theQuery.replace("'", "%22");
-		
-		return getJSONResponseURL(base + theQuery);
+	/*
+	 * Queris the wikidata database for the films in which the actor named @actor played, and 
+	 * the publication data of the film is equal to @year
+	 * 
+	 * @year The publication of the films queried
+	 * @actor The actor who has played in the resulting films
+	 * 
+	 * */
+	public JSONArray queryWithActorAndYear(String year, String actor){
+		String theQuery = queryActorYear.replace("theYear", year).replace("theActor", actor);
+		return getJSONResponseForQuery(theQuery);
+	}
+	
+	/*
+	 * Queries the wikidata for the actors playing in the 
+	 * film whose name specified with input @film
+	 * 
+	 * @film the film name whose actors to be queried
+	 * 
+	 * */
+	public JSONArray queryWithFilm(String film){
+		String theQuery = queryFilm.replace("theFilm", film);
+		return getJSONResponseForQuery(theQuery);
+	}
+	/*
+	 * Queries the wikidata for the films of the actor specified with input @actor
+	 * 
+	 * @actor the actor whose films to be queried
+	 * 
+	 * */
+	public JSONArray queryWithActor(String actor){
+		String theQuery = queryActor.replace("theActor", actor);
+		return getJSONResponseForQuery(theQuery);
 	}
 	
 	public static JSONArray getJSONResponseURL(String url) {
@@ -34,6 +91,7 @@ public class Ozgur_HtmlToJson {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		}
         System.out.println(myString);
         JSONObject myJSONobject = new JSONObject(myString);
