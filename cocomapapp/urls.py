85c513053b93e9b8fcb5af236aadd344ed040229
Kaan Bulut Tekelioglu <kaan@Kaans-MacBook-Pro.local>
2016-11-03 13:10:41 +0300

    Added add_post. Changed topicAdd to trigger POST request.

--- cocomapapp/urls.py
+++ cocomapapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'topic/add', views.add_topic, name='add_topic'),
     url(r'topic/second', views.second_topic, name='second_topic'),
     url(r'topic/math', views.math_topic, name='math_topic'),
+    url(r'topic/postAdd', views.add_post, name='add_post'),
 ]
