f05d232b2b092543183a997fd2dfc5af673ece64
dogukan1905 <dogukanocak05@gmail.com>
2016-11-06 17:47:35 +0200

    Tag, Topic, Post models changed User model added Urls fixed templates fixed

--- cocomapapp/admin.py
+++ cocomapapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Topic, Post, Tag
+from .models import Topic, Post, Tag, User
 
 admin.site.register(Topic)
 admin.site.register(Post)
 admin.site.register(Tag)
+admin.site.register(User)
--- cocomapapp/models.py
+++ cocomapapp/models.py
@@ -3,8 +3,27 @@ from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
+class User(models.Model):
+  first_name = models.TextField()
+  last_name = models.TextField()
+  email = models.EmailField(max_length=70, blank=False, null=False, unique=True)
+  password = models.CharField(max_length=30, blank=False, null=False)
+  created_at = models.DateTimeField(editable=False)
+  updated_at = models.DateTimeField()
+
+  def save(self, *args, **kwargs):
+    if not self.id:
+      self.created_at = timezone.now()
+    self.updated_at = timezone.now()
+    return super(User, self).save(*args, **kwargs)
+
+  def __str__(self):
+    return (str(self.id) + ' ' + self.first_name + ' ' + self.last_name)
+
+@python_2_unicode_compatible
 class Tag(models.Model):
   name = models.TextField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   created_at     = models.DateTimeField(editable=False)
   updated_at    = models.DateTimeField()
 
@@ -20,10 +39,11 @@ class Tag(models.Model):
 @python_2_unicode_compatible
 class Topic(models.Model):
   name = models.TextField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   relates_to = models.ForeignKey('self', null=True, blank=True)
   tags = models.ManyToManyField(Tag, null=True, blank=True)
-  created_at     = models.DateTimeField(editable=False)
-  updated_at    = models.DateTimeField()
+  created_at = models.DateTimeField(editable=False)
+  updated_at = models.DateTimeField()
 
   def save(self, *args, **kwargs):
     if not self.id:
@@ -37,11 +57,13 @@ class Topic(models.Model):
 @python_2_unicode_compatible
 class Post(models.Model):
   content = models.TextField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
   tags = models.ManyToManyField(Tag, null=True, blank=True)
   positive_reaction_count = models.PositiveIntegerField()
   negative_reaction_count = models.IntegerField()
-  created_at     = models.DateTimeField(editable=False)
-  updated_at    = models.DateTimeField()
+  created_at = models.DateTimeField(editable=False)
+  updated_at = models.DateTimeField()
 
   def save(self, *args, **kwargs):
     if not self.id:
