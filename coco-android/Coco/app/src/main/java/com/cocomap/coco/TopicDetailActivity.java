ae29aa71442298ca8b01ce445afe251069f9367f
Emrah <emrah.kucuk@ngier.com>
2016-11-08 17:35:20 +0300

    create topic and add post page UI ready

--- coco-android/Coco/app/src/main/java/com/cocomap/coco/TopicDetailActivity.java
+++ coco-android/Coco/app/src/main/java/com/cocomap/coco/TopicDetailActivity.java
@@ -3,11 +3,11 @@ package com.cocomap.coco;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TopicDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_topic_detail);
     }
 }
