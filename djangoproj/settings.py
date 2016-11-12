90a9d8f30820216289d97064083b5727f42759d6
xoxgur123 <ozgur.akya@gmail.com>
2016-11-12 18:34:41 +0300

    static files problem debug

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -135,7 +135,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = "https://guarded-depths-45847.herokuapp.com/cocomapapp/static/"
+STATIC_ROOT = "https://guarded-depths-45847.herokuapp.com/static/"
 STATIC_URL = os.path.join(PROJECT_ROOT, '../cocomapapp/static/')
 
 # Extra places for collectstatic to find static files.
