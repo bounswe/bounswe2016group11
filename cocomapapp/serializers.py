6f77b25437bccd529969b9386d89259abb340db1
dogukan1905 <dogukanocak05@gmail.com>
2016-11-18 00:34:28 +0200

    ids added to serializers

--- cocomapapp/serializers.py
+++ cocomapapp/serializers.py
@@ -4,26 +4,26 @@ from cocomapapp.models import User, Tag, Topic, Post, Relation
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'password', 'created_at', 'updated_at')
+        fields = ('id', 'first_name', 'last_name', 'email', 'password', 'created_at', 'updated_at')
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('name', 'user', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'user', 'created_at', 'updated_at')
 
 class TopicSerializer(serializers.ModelSerializer):
     posts = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Topic
-        fields = ('name', 'user', 'relates_to', 'tags', 'posts', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'user', 'relates_to', 'tags', 'posts', 'created_at', 'updated_at')
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('content', 'user', 'tags', 'topic', 'positive_reaction_count', 'negative_reaction_count', 'created_at', 'updated_at')
+        fields = ('id', 'content', 'user', 'tags', 'topic', 'positive_reaction_count', 'negative_reaction_count', 'created_at', 'updated_at')
 
 class RelationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Relation
-        fields = ('topic_from','topic_to','label')
+        fields = ('id', 'topic_from','topic_to','label')
