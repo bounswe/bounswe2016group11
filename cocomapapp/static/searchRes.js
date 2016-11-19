374d92e2a5567d447cd228f9f3c0ac46a8fa90b8
bozis <busraozis1994@hotmail.com>
2016-11-19 13:19:46 +0200

    search results developing

--- cocomapapp/static/searchRes.js
+++ cocomapapp/static/searchRes.js
@@ -1,22 +1,31 @@
 $(document).ready(function(){
 //  $("#myButton").click(function(){
        $.getJSON("/static/searchRes.json", function(result){
-          var items = [];
+          //var items = [];
            $.each(result, function(i, obj){
               $.each(obj, function(j, field){
                 $.each(field, function(k, value){
-                      items.push(value.name);
+                                          //console.log(j);
+                      //items.push({j : value.name});
+                    if(j == "topics"){
+                      text = $("<a href=\"\"></a>").text(value.name);
+                      html = $("<li class=\"list-group-item\"></li>").html(text);
+
+                      //html2 = $("<li class=\"list-group-item\"></li>").text(html);
+                      $("#results").append(html);
+                    }else if(j == "posts"){
+                      var text = $("<a href=\"\"></a>").html(value.name); //value.content de olabilir. post a link
+                      var text2 = $("<a href=\"\"></a>").html(value.topicName); //post un ait olduğu topic e link
+                      var text3 = text.add("<br> @ </br>");
+
+                      var html2 = $("<li class=\"list-group-item\"></li>").html(text3.add(text2));
+
+                      $("#results").append(html2);
+                    }
                 });
               });
            });
-           var html =  "";
-           $.each(items, function( index, value ) {
-             text = $("<a href=\"\"></a>").text(value);
-             html = $("<li class=\"list-group-item\"></li>").html(text);
 
-             //html2 = $("<li class=\"list-group-item\"></li>").text(html);
-             $("#results").append(html);
-           });
        });
 //    });
 });
