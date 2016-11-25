55eb58451cc9c5bee63df6351977c0d76585a215
bozis <busraozis1994@hotmail.com>
2016-11-25 18:21:53 +0200

    search results page references added

--- cocomapapp/static/searchRes.js
+++ cocomapapp/static/searchRes.js
@@ -8,15 +8,18 @@ $(document).ready(function(){
                                           //console.log(j);
                       //items.push({j : value.name});
                     if(j == "topics"){
-                      text = $("<a href=\"\"></a>").text(value.name);
+                      ref = "<a href=\"/cocomapapp/topics/"+value.id+"\"></a>";
+                      text = $(ref).text(value.name);
                       html = $("<li class=\"list-group-item\"></li>").html(text);
 
                       //html2 = $("<li class=\"list-group-item\"></li>").text(html);
                       $("#results").append(html);
                     }else if(j == "posts"){
-                      var text = $("<a href=\"\"></a>").html(value.name); //value.content de olabilir. post a link
-                      var text2 = $("<a href=\"\"></a>").html(value.topicName); //post un ait olduğu topic e link
-                      var text3 = text.add("<br> @ </br>");
+                      var ref = "<a href=\"/cocomapapp/topics/"+value.topicId+"\"></a>";  //link to belonging topic (not linked to post directly)
+                      var text = $(ref).html(value.name); //value.content de olabilir. post a link
+                      ref1 = "<a href=\"/cocomapapp/topics/"+value.topicId+"\"></a>"; //again, link to belonging topic
+                      var text2 = $(ref1).html(value.topicName); //post un ait olduğu topic e link
+                      var text3 = text.add("<br> belonging to: </br>");
 
                       var html2 = $("<li class=\"list-group-item\"></li>").html(text3.add(text2));
 
