7cc2ac12ae3a500188fbb0c1506df636faae3bf3
ozgurakyazi <ozgur.akyazi@boun.edu.tr>
2016-12-02 20:18:42 +0300

    UI improvements added, topics' size and arrows' length are imptroved

--- cocomapapp/static/global.js
+++ cocomapapp/static/global.js
@@ -27,7 +27,17 @@ $(function(){
 
             for(var i = 0; i < json_array.length; i++){
               //console.log({id: i+1, label: json_array[i]['name'] })
-              dict1.push({id: json_array[i]['id'], label: json_array[i]['name'] });
+              //Math.random should be replaced
+              //Future FIX
+              var seed = Math.random();
+
+              console.log(seed);
+              var red = Math.round(seed*255);
+              var green = Math.max(Math.round((1-seed)*50),0);
+              var blue = Math.round((1-seed)*255);
+              console.log("red: "+ red+"    , blue:"+blue);
+              dict1.push({id: json_array[i]['id'], size:(20*seed)+20 , font:{size:(25*seed)+10 ,face:'Luckiest Guy',color:'rgb(255,100,0)'},
+              label: json_array[i]['name'],color:'rgb('+red+','+green+','+blue+')' , shape:'circle' });
             }
             var nodes = new vis.DataSet(dict1);
 
@@ -37,11 +47,14 @@ $(function(){
             for(var i = 0; i < json_array.length; i++){
               // this code will be replaced ..............
               for(var j = 0; j < json_array[i]['relates_to'].length; j++){
-                  //console.log(json_array[i]['relates_to'])
-                  //console.log(i, data['topic_from'], data['topic_to']);
-                  //console.log(json_array[i]['relates_to'][j])
-                  dict2.push({from: json_array[i]['relates_to'][j]['topic_from'], to: json_array[i]['relates_to'][j]['topic_to'], arrows:'to',label:json_array[i]['relates_to'][j]['label']  });
-                  //console.log({from: json_array[i]['relates_to'][j], to: i, arrows:'from'  })
+
+                  //length of arrow
+                  console.log(seed);
+                  var arrow_length = Math.round((1-seed)*100+200);
+
+                  dict2.push({ from: json_array[i]['relates_to'][j]['topic_from'], to: json_array[i]['relates_to'][j]['topic_to'], arrows:'to',label:json_array[i]['relates_to'][j]['label'],length: arrow_length });
+                  //dict2.push({from: json_array[i]['relates_to'][j]['topic_from'], to: json_array[i]['relates_to'][j]['topic_to'], arrows:'to',label:json_array[i]['relates_to'][j]['label']  });
+
               }
             }
             var edges = new vis.DataSet(dict2);
@@ -53,7 +66,10 @@ $(function(){
               nodes: nodes,
               edges: edges
             };
-            var options = {};
+            var options = {autoResize: true,
+              height: '100%',
+              width: '100%'
+            };
             var network = new vis.Network(container, data, options);
 
             network.on("click", function (params) {
