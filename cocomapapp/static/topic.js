9715e42f8b9b1c45429578dd4d7703a1e7647d58
ozgurakyazi <ozgur.akyazi@boun.edu.tr>
2016-12-07 01:00:25 +0300

    merge for semantic tagging improvement in topic add page

--- cocomapapp/static/topic.js
+++ cocomapapp/static/topic.js
@@ -1,3 +1,65 @@
+function get_tags(wikiId,theTags){
+  var waitNum;
+  var resultTag;
+  var hidden_tags=[];
+  var props=["P31","P610","P37","P36", "P30","P190","P17","P190",
+  "P27", "P425","P279"];
+  $.ajax({
+    url : "/cocomapapp/wikidataQuery/" +wikiId + "/" ,
+    async:false,
+    dataType:"json"
+  }).fail( function() {
+    console.log("error in wikidata");
+  }).done( function(data) {
+    hidden_tags=get_hidden_tags(props,data);
+    var occupation = get_hidden_tags(["P106"],data);
+    waitNum = occupation.length;
+    $.each(occupation,function(i,value){
+      $.ajax({
+        url : "/cocomapapp/wikidataQuery/" +value + "/" ,
+        async:false,
+        dataType:"json"
+      }).fail( function() {
+        console.log("error in wikidata");
+      }).done( function(data2) {
+        hidden_tags = hidden_tags.concat(get_hidden_tags(["P425"],data2) );
+      });
+    });
+    var result = $.grep(theTags, function(e){ return e.id == wikiId; });
+
+    resultTag = {
+      "id" : wikiId,
+      "label" : result[0].name ,
+      "hidden_tags": hidden_tags
+    };
+  });
+  return resultTag;
+}
+
+function get_hidden_tags(props,the_json){
+  var hidden_tags=[];
+  var sem_att_list=props;
+  $.each(the_json,function(i,value){
+    var temp_url = value.propUrl.value;
+    var cond=false;
+    for(var j=0; j<sem_att_list.length;j++){
+      if(temp_url.endsWith(sem_att_list[j])){
+        cond=true;
+        //console.log(value.propLabel.value);
+        break;
+      }
+    }
+    if(cond){
+      var toRegex = value.valUrl.value;
+      var theRegex = /.*\/([a-z]+?\d+?)$/im ;
+      var fetchedWikiId = theRegex.exec(toRegex)[1];
+      hidden_tags.push(fetchedWikiId);
+    }
+  });
+  //console.log(hidden_tags);
+  return hidden_tags;
+}
+
 function upVote(id){  //For upvoting
 
   //console.log("upVote deyim");
