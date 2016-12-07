6c221772306c406a895ba0a9492374c2f8e6a783
ozgurakyazi <ozgur.akyazi@boun.edu.tr>
2016-12-07 12:25:54 +0300

    now recommendations are generated online with rest api provided

--- cocomapapp/static/recommend.js
+++ cocomapapp/static/recommend.js
@@ -1,21 +1,47 @@
 $(document).ready(function(){
-$.getJSON("/static/recommended.json"
-).fail( function() {
-  console.log("error in recommendation");
-}).done( function(data) {
-
-    recommendationRenderer(data);
+  $.getJSON("/cocomapapp/recommendedPosts"
+  ).fail( function() {
+    console.log("error in recommendation");
+  }).done( function(data) {
+      recommendationRenderer(data);
+  });
 });
+function get_recommended_topics(){
+  var topics;
+  $.ajax({
+    url: "/cocomapapp/recommendedTopics",
+    async: false,
+    dataType:"json"
+  }).done(function(data){
+    topics = data;
+  }).fail(function(){
+    return "Server Connection Failed";
+  });
+  return topics;
+}
+function get_recommended_posts(){
+  var posts;
+  $.ajax({
+    url: "/cocomapapp/recommendedPosts",
+    async: false,
+    dataType:"json"
+  }).done(function(data){
+    posts = data;
+  }).fail(function(){
+    return "Server Connection Failed";
+  });
+  return posts;
+}
 
 function recommendationRenderer(result){
 
-    var topics = result[0].topics;
-    var posts = result[1].posts;
+    var topics = get_recommended_topics();
+    var posts = get_recommended_posts();
     //console.log(topics);
     //console.log(posts);
     $.each(topics,function(i,topic){
       //console.log(topic);
-      ref = "<a href=\"#\">"+topic.name+"</a>";
+      ref = "<a href='topics/"+topic.id +"'>"+topic.name+"</a>";
       html = $("<li class=\"list-group-item\"></li>").html(ref);
 
       $("#recommendedTopics").append(html);
@@ -23,10 +49,9 @@ function recommendationRenderer(result){
 
     $.each(posts,function(i,value){
       //console.log(value.username);
-      ref = "<a href=\"#\">"+value.username+" posted to "+ value.topicName +" <span class=\"sr-only\">(current)</span></a><p>"+value.name+"</p>";
+      ref = "<a href='topics/"+ value.topic.id+ "/'>"+value.user.username+" posted to "+ value.topic.name +" <span class=\"sr-only\">(current)</span></a><p>"+value.content+"</p>";
       html = $("<li class=\"list-group-item\"></li>").html(ref);
 
       $("#recommendedposts").append(html);
     });
 }
-});
