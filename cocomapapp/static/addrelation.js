c16b66abf01f3c4858577ca83977e8115bf2d3e5
ozgurakyazi <ozgur.akyazi@boun.edu.tr>
2016-12-10 19:02:03 +0300

    relation add is done

--- cocomapapp/static/addrelation.js
+++ cocomapapp/static/addrelation.js
@@ -3,8 +3,6 @@ get_topic_list();
 var relations=[];
 
 $(document).ready(function () {
-
-
   $('#relates_to').selectize({
       maxItems: 1,
       maxOptions: 5,
@@ -47,6 +45,50 @@ $(document).ready(function () {
       }
   });
 
+  $("#submit2").on("click", function() {
+    var relations =[];
+    if($('#relates_to').val() && $('#relates_to').val() != topic_id){
+      var relation={};
+      relation.topic_from = topic_id;
+      relation.topic_to = $('#relates_to').val();
+      relation.label = $('#relationships-name').val();
+      relations.push(relation);
+    }
+    if($('#relates_to2').val() && $('#relates_to2').val() != topic_id){
+      var relation2={};
+      relation2.topic_from = topic_id;
+      relation2.topic_to = $('#relates_to2').val();
+      relation2.label = $('#relationships-name2').val();
+      relations.push(relation2);
+    }
+    if($('#relates_to3').val()&& $('#relates_to3').val() != topic_id){
+      var relation3={};
+      relation3.topic_from = topic_id;
+      relation3.topic_to = $('#relates_to3').val();
+      relation3.label = $('#relationships-name3').val();
+      relations.push(relation3);
+    }
+    if(relations.length ==0){
+      return;
+    }
+    
+    $.ajax({
+        url: 'add',
+        type: 'POST',
+        data:JSON.stringify(topic),
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            window.location.href = "/cocomapapp";
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+  });
+  $('#cancel_bt').click(function(){
+  		parent.history.back();
+  		return false;
+  });
 
 });
 
@@ -62,6 +104,7 @@ function get_topic_list(){
         name: value.name
       });
     });
+
     return;
   }).fail(function(){
     return "Server Connection Failed";
