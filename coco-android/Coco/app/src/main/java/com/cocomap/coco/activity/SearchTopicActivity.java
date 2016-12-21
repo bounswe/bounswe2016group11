ef0bb469cb6e13eed5be87b4db91ee1e6fe6edb0
Sinan Harputluoglu <sinanharputluoglu@gmail.com>
2016-12-21 13:24:06 +0300

    Search topic activity added

---
+++ coco-android/Coco/app/src/main/java/com/cocomap/coco/activity/SearchTopicActivity.java
@@ -0,0 +1,19 @@
+package com.cocomap.coco.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sinan on 21.12.2016.
+ */
+public class SearchTopicActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
